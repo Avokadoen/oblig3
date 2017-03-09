71553d950186be57d85d81902af0433e7fb87ed6
hallajs <Akselhallajs@gmail.com>
2017-03-09 18:16:20 +0100

    Progress

--- LegeResept.h
+++ LegeResept.h
@@ -1,22 +1,76 @@
 #ifndef LegeResept
 #define LegeResept
 
+//		LISTTOOL
 #include "ListTool2A.h"
 
+//		GLOBALE VARIABLER
+const int MAXTXT = 80;
+const int MAXRSPT = 50;
+const int MAXLEGR = 100;
+
+//		KLASSER
 class Resept : NumElement  {
 private:
 	char* pasientNavn;
 	char* medisinNavn;
 	int dosering;
+public:
+	Resept() {
+		number = lesDato();
+		les("\nPasientnavn: ", pasientNavn);
+		// sp¯r om legenavn
+		// hvis finnes legg resept i liste
+		// ellers lag ny lege og legg i liste
+		les("\nMedisintype: ", medisinNavn);
+
+	}
 };
 
 class Lege : TextElement {
 private:
+	Resept* resepter[MAXRSPT];
 	char* navn;
 	char* adresse;
 	int telefon;
-
+	int antResepter;
 };
 
+//		LESFUNKSJONER
 
+// Les inn pointer tekst
+void les(char* skriv, char* input) {
+	char temp[MAXTXT];
+	cout << skriv;
+	cin.getline(temp, MAXTXT);
+	input = new char[strlen(temp) + 1];
+	strcpy_s(input, MAXTXT, temp);
+}
+
+// les inn en karakter
+char les(char* skriv) {
+	char temp;
+	cout << skriv; cin >> temp;
+	return (toupper(temp));
+}
+
+// les inn tall
+int les(char* skriv, int min, int max) {
+	int temp;
+	do {
+		cout << "\n\t" << min << " til " << max << endl;
+		cout << skriv; cin >> temp;
+	} while (temp < min && temp > max);
+
+	return temp;
+}
+
+// Leser inn dato for resept (indirekte)
+int lesDato() {
+	int temp≈r, tempMÂn, tempDag;
+	temp≈r = les("\nReseptaar: ", 2015, 2025);
+	tempMÂn = les("\nReseptmaaned: ", 1, 12);
+	tempDag = les("\nReseptdag: ", 1, 31);
+	return((temp≈r * 10000) + (tempMÂn * 100) + tempDag);
+}
 #endif
--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,96 @@
-#include <iostream>				// cin, cout
-//#include "ListTool2A.h"		// 
-#include "LegeResept.h"			// class Lege, resept og ListTool2A.h
+#include <iostream>				// cin, cout					  |
+//#include "ListTool2A.h"		//								  |
+#include "LegeResept.h"			// Lege, resept, les, ListTool2A.h|
+								//--------------------------------|		
 
 
-//					GLOBALE CONST VARIABLER
-const int MAXTXT = 80;
+
 
 //					GLOBALE FUNKSJONER
 void hovedMeny();
 void gjentager(char symbol, int lengde);
 
-void les(char* t, char* input);
+//					OBJEKT
+Lege* leger[MAXLEGR];
 
+// 
+int antLeger;
 
+//					MAIN
 int main() {
+	antLeger = 0;
+
+	char kommando;
+
 	hovedMeny();
-	system("pause");
+	
+	do {
+		kommando = les("\nMenyvalg: ");
+		switch (kommando) {
+		case 'R': 
+			
+			char* tempLegeNavn;
+			//Bekreft valgt kommando
+			cout << endl;
+			gjentager('-', 19); cout << " NY RESEPT "; gjentager('-', 19);
+			//	lag en ny resept
+			Resept* temp = new Resept; 
+		
+
+			//	Be om dato(i format ÅÅÅÅMMDD i interval 2015 - 2025, 01 - 12, 0 - 31) //lesfunk*
+			//	Be om pasientens navn(kanskje sjekke om den har "x y")
+			//	Be om Legens navn
+			les("\nReseptgiver (navn): ", tempLegeNavn);
+			for (int i = 0; i < antLeger; i++) {
+				leger[i]->inList(tempLegeNavn);
+			}
+			//	if (eksisterer) { (sett objekt inn i lege objekt og sorter)*funk ? }
+			//	else { ny lege(-|| -)*funk ? }
+			//	Be om medisinens navn(Det er ialt ca. 50 forskjellige narkotiske stoffer ? )
+			//		Be om dosering i milligram(intervall 1 - 1000)
+			
+			break;
+
+		case 'L':
+			break;
+		case 'P':
+			break;
+		case 'F':
+			break;
+		case 'U':
+			break;
+		case 'I':
+			break;
+
+		case 'H': hovedMeny(); break; // Kaller på meny
+
+		case 'Q':					  // Avslutter program
+			char avsluttSvar;
+			do{	// Går ikke ut av case før man har svart J
+
+				// Bruker blir spurt om å lagre
+				avsluttSvar = les("\n\t\tAvslutter program\n\n Onsker du aa lagre (j | n): ");
+				if (avsluttSvar == 'J') {
+					cout << "\n\nLagrer til fil..";
+					// #lagre fil
+				}
+
+				else {
+					// Hvis bruker velgger å ikke lagre så må han/hun bekrefte
+					avsluttSvar = les("\nEr du sikker?(j | n): ");
+				}
+			} while (avsluttSvar != 'J');
+			break;
+
+		}
+	} while (kommando != 'Q');
+
+	// Avslutter program
+	cout << "\nHa en fin dag videre.";
 	return 0;
 }
 
+//
 void hovedMeny() {
 	cout << endl;
 	cout << "\t"; gjentager('-', 20); cout << "HOVEDMENY"; gjentager('-', 20);
@@ -28,7 +100,6 @@ void hovedMeny() {
 		 << "\tP = Lag oversikt over resepter til en bestemt pasient\n"
 		 << "\tF = Fjern alle gamle resepter\n"
 		 << "\tU = Skriv ut til fil\n"
-		 << "\tP = Lag oversikt over resepter til en bestemt pasient\n"
 		 << "\tI = Les inn fra fil \n"
 		 << "\tH = Hjelp\n"
 		 << "\tQ = Quit \n";
@@ -38,11 +109,5 @@ void gjentager(char symbol, int lengde) {
 	for (int i = 0; i < lengde; i++)
 		cout << symbol;
 }
+//
 
-void les(char* tekst, char* input) {
-	char temp[MAXTXT];
-	cout << tekst;
-	cin.getline(temp, MAXTXT);
-	input = new char[strlen(temp) + 1];
-	strcpy_s(input, MAXTXT, temp);
-}
